ea4324f2ff159315e9d686944fca63d637030ea4
Christi Kerr <christinakerr394@gmail.com>
2020-11-14 11:48:37 -0600

    Built list of static files to pre-cache

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const iconSizes = ["192", "512"];
+const iconFiles = iconSizes.map(
+    (size) => `/icons/icon-${size}x${size}.png`
+);
+
+const staticFilesToPreCache = [
+    "/",
+    "/index.js",
+    "/db.js",
+    "/favicon.ico",
+    "/manifest.webmanifest"
+].concat(iconFiles);
+
+self.addEventListener("install", function (evt) {
+    
+})
