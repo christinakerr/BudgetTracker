9403186c6b94a630d0ee54f90d8422f6cb351e6d
Christi Kerr <christinakerr394@gmail.com>
2020-11-14 11:58:32 -0600

    Added function to fetch from the cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -39,3 +39,31 @@ self.addEventListener("activate", function (evt) {
     )
     self.clients.claim();
 });
+
+self.addEventListener("fetch", function(evt) {
+    const {url} = evt.request;
+    if (url.includes("/api")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                .then(response => {
+                    if(response.status === 200){
+                        cache.put(evt.request, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                })
+            }).catch(err => console.log(err))
+        );
+    } else {
+        evt.respondWith(
+            caches.open(CACHE_NAME).then(cache => {
+                return cache.match(evt.request).then(response => {
+                    return response || fetch(evt.request);
+                })
+            })
+        )
+    }
+})
