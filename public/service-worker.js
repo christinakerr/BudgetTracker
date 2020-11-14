c14d9f9accd465aab8a5176a6ef517b2411035ab
Christi Kerr <christinakerr394@gmail.com>
2020-11-14 11:50:23 -0600

    Built event listener to install service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,5 +15,15 @@ const staticFilesToPreCache = [
 ].concat(iconFiles);
 
 self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(staticFilesToPreCache);
+        })
+    );
+    self.skipWaiting();
+})
+
+self.addEventListener("activate", function (evt) {
     
 })
